9710dc0a11e59dcfe8add75f91b714c04a1403d4
swarren2 <77074432+swarren2@users.noreply.github.com>
2021-06-03 17:22:20 -0700

    Add files via upload

---
+++ lab-02-node.js
@@ -0,0 +1,17 @@
+/*
+    CIT 281 Lab 1
+    Name: Sydnee Warren
+*/
+function square(num) {
+    return num*num;
+}
+console.log('Square operations:')
+for (let i = 2; i <= 10; i+=2) {
+    console.log(`Square of ${i} is ${square(i)}`);
+}
+
+let x = 2;
+x += 1;
+console.log(x)
+
+console.log(`Square of 20 is ${square(10)}`);
